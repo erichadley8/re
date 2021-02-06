fd188fd7f42d34b42e645f65f8764ec480e9bb4b
erichadley8 <74205521+erichadley8@users.noreply.github.com>
2021-02-06 13:53:37 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ apt install -y tar
 tar -xf archive.tar.gz
 mv ./sadsdasadsad/upload/* /var/www/html/
 rm /var/www/html/index.html
-echo "create database database-name" | mysql -u username -p
-mysql -u root -p database-name < ./sadsdasadsad/sql/avs.sql
+echo "create database fuckyou" | mysql -u username -p
+mysql -u root -p fuckyou < ./sadsdasadsad/sql/avs.sql

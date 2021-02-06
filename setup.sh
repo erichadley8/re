b9496fd980604525bb938a89da071f5b6624fd18
erichadley8 <74205521+erichadley8@users.noreply.github.com>
2021-02-06 12:56:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ apt install -y mysql-server
 apt install -y apache2
 apt install -y php libapache2-mod-php php-mysql
 apt install -y tar
-tar archive.tar.gz
+tar -xf archive.tar.gz
 mv ./sadsdasadsad/upload/* /var/www/html/
 rm /var/www/html/index.html
+mysql -u root -p < "create database avs"
+mysql -u root -p db < avs.sql

e2a6abc139252659627f338556e4f61dcac9bd2b
erichadley8 <74205521+erichadley8@users.noreply.github.com>
2021-02-06 12:40:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ apt install -y apache2
 apt install -y php libapache2-mod-php php-mysql
 apt install -y tar
 tar archive.tar.gz
+mv ./sadsdasadsad/upload/* /var/www/html/
+rm /var/www/html/index.html
